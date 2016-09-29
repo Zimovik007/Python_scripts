988ed211f4071677583af7083885562a272b9535
Zimovik007 <zimovik@list.ru>
2016-09-29 12:54:53 +1000

    Script to read the GET parameters and add them to the database table

---
+++ python_script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os, sys, cgi
+import MySQLdb
+
+print "Content-Type: text/html; charset=UTF8\n\n"
+
+def check_param(template):
+    try:
+        temp = form[template].value
+    except:
+        temp = ''
+    return temp
+
+form = cgi.FieldStorage()
+reference = check_param("reference")
+message   = check_param("message")
+code      = check_param("code")
+number    = check_param("number")
+price     = check_param("price")
+timestamp = check_param("timestamp")
+mccmnc    = check_param("mccmnc")
+split     = check_param("split")
+
+#the variables below need to fill out
+#table_name - the variable in which to record the name of the table from the database
+
+db_server  = ""
+db_login   = ""
+db_pass    = ""
+db_name    = ""
+table_name = ""
+
+try:
+    dbh = MySQLdb.connect(db_server, db_login, db_pass, db_name)
+    cursor = dbh.cursor()
+    query = "INSERT INTO "+ table_name +" VALUES(NULL,"+str(reference)+","+int(number)+","+int(code)+","+str(message)+","+float(price)+","+timestamp+","+int(mccmnc)+","+int(split)+")"
+    cursor.execute(query)
+    dbh.commit()
+except:
+    print "failure to connect"
